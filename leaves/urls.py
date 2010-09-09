e8bafac1e9ee5d7cc84df38bc82d916866c20edd
unknown <szke418694@.(none)>
2010-09-09 14:33:11 +0300

    The urls.py for leave was left out too, Oooops!

--- leaves/urls.py
+++ leaves/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls.defaults import * 
 from hrms.leaves.models import LeaveApplication  
 
-#info_dict = {
-#    'queryset': Poll.objects.all(),
-#}
+info_dict = {
+    'queryset': LeaveApplication.objects.all()
+}
 
 urlpatterns = patterns('hrms.leaves.views', 
     (r'^$', 'leaves_home'),
+    (r'^period/(?P<period_id>\d{1,4})/$', 'leaves_in_period'), 
 )
 
 urlpatterns += patterns('django.views.generic.list_detail', 
     # (r'^$', 'object_list', info_dict),
-    #(r'^(?P<object_id>\d+)/$', 'object_detail', info_dict), 	
+    (r'^(?P<object_id>\d+)/details/$', 'object_detail', info_dict), 	
     # url(r'^(?P<object_id>\d+)/results/$', 'object_detail', dict(info_dict, template_name='polls/results.html'), 'poll_results'),
 )
 
